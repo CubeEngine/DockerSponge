15c939aece3bc43cc7bc3e74f17e7fee3a99cc4a
Phillip Schichtel <phillip@schich.tel>
2021-07-03 18:23:00 +0200

    supply localhost IP explicitly to be on the safe side

--- mcrcon.sh
+++ mcrcon.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+export MCRCON_HOST='127.0.0.1'
 export MCRCON_PORT="$(grep '^rcon.port=' "$MINECRAFT_DIR/server.properties" | cut -d'=' -f 2-)"
 export MCRCON_PASS="$(grep '^rcon.password=' "$MINECRAFT_DIR/server.properties" | cut -d'=' -f 2-)"
 
